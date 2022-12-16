e5f42741ebcefd434882982bf9b17e0622adc990
lgonzalez-extraspace <120606089+lgonzalez-extraspace@users.noreply.github.com>
2022-12-16 13:48:50 -0700

    1 file modified, 6 added, 6 deleted

--- components/TaxFilters.tsx
+++ components/TaxFilters.tsx
@@ -1,7 +1,7 @@
 import { uniq } from 'lodash';
 import * as React from 'react';
-import './styles/style.css';
-import { data } from './data/data';
+import '../styles/style.css';
+import { data } from '../data/data';
 
 export default function FeeFilters({
   usStateChanged,
