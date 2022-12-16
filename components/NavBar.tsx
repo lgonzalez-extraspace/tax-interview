925b839eb40963e821302e9b3d502adfc67c0714
lgonzalez-extraspace <120606089+lgonzalez-extraspace@users.noreply.github.com>
2022-12-16 14:06:28 -0700

    2 files modified, 3 added, 3 deleted

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -6,6 +6,9 @@ export default function NavBar() {
   return (
     <div className="navbar">
       <div className="navbar-logo"></div>
+      <div className="navbar-spacer top"></div>
+      <div className="admin-title">Administration</div>
+      <div className="navbar-spacer bottom"></div>
       <div className="navbar-links">
         <div className="navbar-link active">Taxes</div>
         <div className="navbar-link">Discounts</div>
